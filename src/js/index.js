9883b73421b2513c268ac741e47cb7111610df17
StefanoTorres <sptorres@ualberta.com>
2021-02-07 18:19:37 -0700

    Created index and load event
    created a rudementart index.html page and added load event to index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,7 @@
 // free Alpha vantage API Key is FGGOEPBHRR7EH0L2
+
+window.addEventListener('load',function(e){
+    const searchURL = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&interval=5min&outputsize=full&apikey=FGGOEPBHRR7EH0L2&symbol=";
+
+    
+})
